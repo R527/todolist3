4949892e517974ac8cc8b7806fcea3eb8075c9d3
R <rockleeserver3@gmail.com>
2022-01-23 21:30:24 +0900

    main.javaにて エラーあり

--- app/src/main/java/com/example/todolist3/MainActivity.java
+++ app/src/main/java/com/example/todolist3/MainActivity.java
@@ -1,172 +1,54 @@
 package com.example.todolist3;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.LinearLayout;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.BaseAdapter;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    String str = "0";
-    String storageNum = "";
-    String calculation;
-    TextView tv;
+
+
+
+    private static final String[] names = {
+            "Yuka",
+            "Kurumi",
+            "Tomoya",
+            "Mai",
+            "Miki",
+            "Saya",
+            "Toko",
+            "Nagi",
+            "Yuyu",
+            "Yumiko",
+            "Katakuriko"
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.v("LifeCycle","onCreate");
 
-        Button btn0 = (Button)findViewById(R.id.btn0);
-        Button btn1 = (Button)findViewById(R.id.btn1);
-        Button btn2 = (Button)findViewById(R.id.btn2);
-        Button btn3 = (Button)findViewById(R.id.btn3);
-        Button btn4 = (Button)findViewById(R.id.btn4);
-        Button btn5 = (Button)findViewById(R.id.btn5);
-        Button btn6 = (Button)findViewById(R.id.btn6);
-        Button btn7 = (Button)findViewById(R.id.btn7);
-        Button btn8 = (Button)findViewById(R.id.btn8);
-        Button btn9 = (Button)findViewById(R.id.btn9);
-        Button btn_eqales = (Button)findViewById(R.id.btn_eqales);
-        Button btn_kakeru = (Button)findViewById(R.id.btn_kakeru);
-        Button btn_mainasu = (Button)findViewById(R.id.btn_mainasu);
-        Button btn_waru = (Button)findViewById(R.id.btn_pasento);
-        Button btn_tasu = (Button)findViewById(R.id.btn_tasu);
-        tv = (TextView)findViewById(R.id.Textview);
+        String[] emails = new String[names.length];
 
-        btn0.setOnClickListener(v -> {
-            Log.v("btnTest","btn0");
-            tv.setText(setTextNum("0"));
-        });
+        // nameからメルアド作成
+        for(int i=0; i< names.length ;i++ ){
+            emails[i] = String.format(Locale.US, "%s@mail.co.jp",names[i]);
 
-        btn1.setOnClickListener(v -> {
-            Log.v("btnTest","btn1");
-            tv.setText(setTextNum("1"));
-        });
-
-        btn2.setOnClickListener(v -> {
-            Log.v("btnTest","btn2");
-            tv.setText(setTextNum("2"));
-        });
-
-        btn3.setOnClickListener(v -> {
-            Log.v("btnTest","btn3");
-            tv.setText(setTextNum("3"));
-        });
-
-        btn4.setOnClickListener(v -> {
-            Log.v("btnTest","btn4");
-            tv.setText(setTextNum("4"));
-        });
-
-        btn5.setOnClickListener(v -> {
-            Log.v("btnTest","btn5");
-            tv.setText(setTextNum("5"));
-        });
-
-        btn6.setOnClickListener(v -> {
-            Log.v("btnTest","btn6");
-            tv.setText(setTextNum("6"));
-        });
-
-        btn7.setOnClickListener(v -> {
-            Log.v("btnTest","btn7");
-            tv.setText(setTextNum("7"));
-        });
-
-        btn8.setOnClickListener(v -> {
-            Log.v("btnTest","btn8");
-            tv.setText(setTextNum("8"));
-        });
-
-        btn9.setOnClickListener(v -> {
-            Log.v("btnTest","btn9");
-            tv.setText(setTextNum("9"));
-        });
-
-
-
-        btn_eqales.setOnClickListener(v -> {
-            Log.v("btnTest","btn_eqales");
-            int i = 0;
-            int val1 = Integer.valueOf(storageNum);
-            int val2 = Integer.valueOf(str);
-            System.out.println(calculation);
-            switch (calculation){
-
-                case "+":
-                    i = val1 + val2;
-                    System.out.println("eqalesTest" + val1);
-                    System.out.println("eqalesTest" + val2);
-                    break;
-                case "-":
-                    i = val1 - val2;
-                    break;
-                case "/":
-                    i = val1 / val2;
-                    System.out.println("eqalesTest" + val1);
-                    System.out.println("eqalesTest" + val2);
-                    break;
-                case "*":
-                    i = val1 * val2;
-                    System.out.println("eqalesTest" + val1);
-                    System.out.println("eqalesTest" + val2);
-                    break;
-            }
-            System.out.println("合計" + i);
-            System.out.println("クラス" + Integer.toString(i).getClass().getSimpleName());
-            tv.setText(Integer.toString(i));
-            str = "";
-            storageNum = "";
-        });
-
-        btn_kakeru.setOnClickListener(v -> {
-            Log.v("btnTest","btn_kakeru");
-            setCalculation("*");
-        });
-
-        btn_mainasu.setOnClickListener(v -> {
-            Log.v("btnTest","btn_mainasu");
-            setCalculation("-");
-        });
-
-        btn_waru.setOnClickListener(v -> {
-            Log.v("btnTest","btn_waru");
-            setCalculation("/");
-        });
-
-        btn_tasu.setOnClickListener(v -> {
-            Log.v("btnTest","btn_tasu");
-            setCalculation("+");
-        });
-    }
-
-    public String setTextNum(String inputNum){
-        String result = "0";
-        if(str.equals("0")){
-            result = inputNum;
-        }else{
-            result = str + inputNum;
         }
-        str = result;
-        return result;
+
+        // ListViewのインスタンスを生成
+        ListView listView = findViewById(R.id.listView);
+
+        // BaseAdapter を継承したadapterのインスタンスを生成
+        // レイアウトファイル list_items.xml を
+        // activity_main.xml に inflate するためにadapterに引数として渡す
+        BaseAdapter adapter = new TestAdapter(this.getApplicationContext(), names, emails);
+
+
+        // ListViewにadapterをセット
+        listView.setAdapter(adapter);
+
     }
-
-    public void setCalculation(String inputCalculation){
-        storageNum = str;
-        str = "";
-        tv.setText("0");
-        calculation = inputCalculation;
-
-
-    }
-
-
-
 }
---
+++ app/src/main/java/com/example/todolist3/TestAdapter.java
@@ -0,0 +1,87 @@
+package com.example.todolist3;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class TestAdapter extends BaseAdapter {
+
+    private LayoutInflater inflater;
+    private int layoutID;
+    private String[] namelist;
+    private String[] emaillist;
+    private Bitmap[] photolist;
+
+    static class ViewHolder {
+        TextView text;
+        TextView email;
+        ImageView img;
+    }
+
+    void TestAdapter(Context context,
+                String[] names, String[] emails){
+
+        inflater = LayoutInflater.from(context);
+//        layoutID = itemLayoutId;
+
+        namelist = names;
+        emaillist = emails;
+        // bitmapの配列
+//        photolist = new Bitmap[photos.length];
+        // drawableのIDからbitmapに変換
+//        for( int i = 0; i< photos.length; i++){
+//            photolist[i] = BitmapFactory.decodeResource(context.getResources(), photos[i]);
+//        }
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder holder;
+
+        if (convertView == null) {
+            convertView = inflater.inflate(layoutID, null);
+            holder = new ViewHolder();
+            holder.img = convertView.findViewById(R.id.img_item);
+            holder.text = convertView.findViewById(R.id.text_view);
+            holder.email = convertView.findViewById(R.id.text_mail);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.img.setImageBitmap(photolist[position]);
+
+        String str = "Staff ID.170900"+ String.valueOf(position+1)
+                + "\n\nEmail: " + emaillist[position]
+                + "\nTel: 020-8931-9933 #340" + String.valueOf(position+1);
+        holder.email.setText(str);
+
+        holder.text.setText(namelist[position]);
+
+        return convertView;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+
+}
